3309eea21ba9ed52d7affb309f11066bee2acbea
Dmitrii Kudriavtcev <Dmitrii_Kudriavtcev@epam.com>
2016-10-06 18:57:21 +0300

    initial commit

---
+++ WSDLSorter/WSDLSorter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSDLSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+                return;
+
+            var wsdlSort = new WsdlSort(args[0]);
+
+            wsdlSort.LoadXml();
+            wsdlSort.SaveSortedXml(args[1]);
+        }
+    }
+}
---
+++ WSDLSorter/WSDLSorter/WSDLSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace WSDLSorter
+{
+    public class WsdlSort
+    {
+        private readonly string _fileName;
+        private readonly XmlDocument _xmlDocument;
+
+        public WsdlSort(string fileName)
+        {
+            _fileName = fileName;
+            _xmlDocument = new XmlDocument();
+        }
+
+        public void LoadXml()
+        {
+            _xmlDocument.Load(_fileName);
+            SortDocument();
+        }
+
+        public void SaveSortedXml(string outFileName)
+        {
+            _xmlDocument.Save(outFileName);
+        }
+
+        private void SortDocument()
+        {
+            // TODO sort xml
+        }
+    }
+}
